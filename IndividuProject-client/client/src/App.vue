ba965d36116e75d73b3876bb82e8f7060c842c6f
Okta Falova <34345603+avolaf8@users.noreply.github.com>
2023-06-25 11:50:56 +0700

    Add additional

---
+++ IndividuProject-client/client/src/App.vue
@@ -0,0 +1,21 @@
+<script >
+import { mapActions, mapState, mapWritableState } from 'pinia';
+import { RouterLink, RouterView } from 'vue-router'
+import { useAppStore } from './stores/app';
+
+export default {
+  computed: {
+    ...mapWritableState(useAppStore, ['isAuth'])
+  },
+  created() {
+    if (localStorage.access_token)
+      this.isAuth = true
+  }
+}
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style></style>
