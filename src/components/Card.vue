1d5100e29c0cb25035a6b7e6678766b38d3fef20
Okta Falova <34345603+avolaf8@users.noreply.github.com>
2023-06-25 11:35:42 +0700

    Add Client-side
    Frontend

---
+++ src/components/Card.vue
@@ -0,0 +1,42 @@
+<script>
+import { mapActions, mapState } from "pinia";
+import { useAppStore } from "../stores/app";
+
+export default {
+  props: ["product"],
+  computed: {
+    ...mapState(useAppStore, ["isAuth"]),
+  },
+  methods: {
+    ...mapActions(useAppStore, ["handlerAddCart"]),
+    addCartHandler() {
+      this.handlerAddCart(this.product.id, this.product.thirdAPI, this.product.card_prices[0].ebay_price);
+    },
+  },
+  created() {
+
+  },
+};
+</script>
+
+<template>
+  <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 u-s-m-b-30 filter__item outwear">
+    <div class="product-bs">
+      <div class="product-bs__container">
+        <div class="product-bs__wrap">
+          <a class="aspect aspect--bg-grey aspect--square u-d-block" v-if="isAuth" @click.prevent="addCartHandler">
+            <a data-modal="modal" data-modal-id="#add-to-cart"><i class="fas fa-plus-circle"></i></a>
+            <img class="aspect__img" :src="product.card_images[0].image_url" alt=""></a>
+          <div class="product-r__action-wrap">
+          </div>
+          <span class="product-bs__name">{{ product.name }}</span>
+          <div class="product-bs__rating gl-rating-style"><i class="fas fa-star"></i><i class="fas fa-star"></i><i
+              class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>
+          </div>
+          <span class="product-bs__price">${{ product.card_prices[0].ebay_price }}</span>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,66 @@
+<script>
+import { mapActions, mapState } from 'pinia';
+import { useAppStore } from '../stores/app';
+
+export default {
+  computed: {
+    ...mapState(useAppStore, ['isAuth'])
+  },
+  methods: {
+    ...mapActions(useAppStore, ['logout']),
+    logoutHandler() {
+      this.logout({
+      })
+    }
+  }
+}
+</script>
+
+<template>
+  <header class="header--style-2">
+    <nav class="primary-nav-wrapper">
+      <div class="container">
+        <div class="primary-nav">
+          <a class="main-logo" @click.prevent="$router.push('/')">
+            <img src="../assets/images/logo/logo-2.png" alt=""></a>
+          <form class="main-form">
+            <label for="main-search"></label>
+            <input class="input-text input-text--border-radius input-text--style-2" type="text" id="main-search"
+              placeholder="Search">
+            <button class="btn btn--icon fas fa-search main-search-button" type="submit"></button>
+          </form>
+          <div class="menu-init" id="navigation">
+            <div class="ah-lg-mode">
+              <span class="ah-close">✕ Close</span>
+              <ul class="ah-list ah-list--design1 ah-list--link-color-white">
+                <a class="mini-cart-shop-link" v-if="isAuth" @click.prevent="$router.push('/checkout')"><i
+                    class="fas fa-shopping-bag"></i></a>
+                <li class="has-dropdown" data-tooltip="tooltip" data-placement="left" title="Account">
+                  <a><i class="far fa-user-circle"></i></a>
+                  <span class="js-menu-toggle"></span>
+                  <ul style="width:120px">
+
+                    <li>
+                      <a href="signup.html" @click.prevent="$router.push('/register')"><i
+                          class="fas fa-user-plus u-s-m-r-6"></i>
+                        <span>Signup</span></a>
+                    </li>
+                    <li>
+                      <a href="signin.html" v-if="!isAuth" @click.prevent="$router.push('/login')"><i
+                          class="fas fa-lock u-s-m-r-6"></i>
+                        <span>Signin</span></a>
+                    </li>
+                    <li>
+                      <a href="signup.html" v-if="isAuth" @click.prevent="logoutHandler">
+                        <i class="fas fa-lock-open u-s-m-r-6"></i>
+                        <span>Signout</span></a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+  </nav>
+</header></template>
